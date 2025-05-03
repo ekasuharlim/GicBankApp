a725217320f90e00259664da01270c3ff7275260
eka <kia@DESKTOP-TCD7EKJ>
2025-05-03 11:36:23 +0700

    Eod balance calculation

--- GicBankApp/Shared/Error.cs
+++ GicBankApp/Shared/Error.cs
@@ -17,10 +17,14 @@ public class Error
     public static Error InsufficentBalance =
         new("BANKACCOUNT.INSUFFICENT_BALANCE", "Insufficent Balance.");
 
+    public static Error TransactionDateMustBeAfterLastTransaction =
+        new("BANKACCOUNT.TXN_DATE_MUST_BE_AFTER_LAST_TXN", "Transaction date must be the same or after the last transaction date.");
+
     public static Error InvalidInterestRateAmount =
         new("INTERESTRULE.INVALID_RATE_AMOUNT", "Interest rate should be greater than 0 and less than 100");
 
     public static Error InvalidMonthlyPeriodMonth =
         new("MONTHLYPERIOD.INVALID_MONTH", "Monthly Period Month must be between 1 and 12");
+
 
 }
